7bb9be8f9324204b2368f5f88ab494c44f2a606c
Vasil Hristov <vasil.hristov@netea.bg>
2022-02-26 02:48:59 +0200

    Done tasks from practicum 1

---
+++ cmake-build-debug/CMakeFiles/OOP_2022_tasks.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_2022_tasks.dir/Structures/struct_practicum1.cpp.obj"
+  "OOP_2022_tasks.exe"
+  "OOP_2022_tasks.exe.manifest"
+  "OOP_2022_tasks.lib"
+  "OOP_2022_tasks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_2022_tasks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
